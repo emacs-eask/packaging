5246f5f60a589876331de551c159ee953a9a4887
Jen-Chieh Shen <jcs090218@gmail.com>
2022-05-11 01:57:24 +0800

    done

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -11,16 +11,9 @@ $packageArgs = @{
   url           = $url
   url64bit      = $url64
 
-  softwareName  = 'eask*'
-
   checksum      = '721AC91BF248D69C98538BA4C90AC7C804D5F3E0BFFAB15F6986067D2F3446CB'
   checksumType  = 'sha256'
   checksum64    = '721AC91BF248D69C98538BA4C90AC7C804D5F3E0BFFAB15F6986067D2F3446CB'
-  checksumType64= 'sha256'
-
-  # MSI
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
-  validExitCodes= @(0, 3010, 1641)
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyZipPackage @packageArgs
