eacad6846b9ca2071d1d4a05b83324fed744197d
JenChieh <jcs090218@gmail.com>
2023-10-10 23:07:31 -0700

    Try remove version

--- Formula/eask-cli.rb
+++ Formula/eask-cli.rb
@@ -3,11 +3,10 @@ require "language/node"
 class EaskCli < Formula
   name "Eask CLI"
   desc "A set of command-line tools to build Emacs packages"
-  version "0.8.3"
   homepage "https://emacs-eask.github.io/"
   license "GPLv3"
 
-  url "https://registry.npmjs.org/@emacs-eask/-/cli-#{version}.tgz"
+  url "https://registry.npmjs.org/@emacs-eask/-/cli-0.8.3.tgz"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
   depends_on "node"
