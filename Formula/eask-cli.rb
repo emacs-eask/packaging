e3544c84615eadc72c8a0935f224ff2918674acc
JenChieh <jcs090218@gmail.com>
2023-10-10 23:11:02 -0700

    fix url

--- Formula/eask-cli.rb
+++ Formula/eask-cli.rb
@@ -6,13 +6,12 @@ class EaskCli < Formula
   homepage "https://emacs-eask.github.io/"
   license "GPLv3"
 
-  url "https://registry.npmjs.org/@emacs-eask/-/cli-0.8.3.tgz"
+  url "https://registry.npmjs.org/@emacs-eask/cli/-/cli-#{version}.tgz"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    bin.install "cli-#{version}"
   end
 end
