9f6f0d3dc57339efd3c034016e382397b6897454
JenChieh <jcs090218@gmail.com>
2023-10-10 22:09:47 -0700

    Add hash

--- Formula/eask-cli.rb
+++ Formula/eask-cli.rb
@@ -1,6 +1,7 @@
 cask "eask-cli" do
   arch arm: "arm64", intel: "x64"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 arm: "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+         intel: "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
   name "Eask CLI"
   desc "A set of command-line tools to build Emacs packages"
