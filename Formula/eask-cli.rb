fe0c4ff6ce9858d171a025bb86393a6df90d2100
JenChieh <jcs090218@gmail.com>
2023-10-10 22:07:28 -0700

    fix symbol

--- Formula/eask-cli.rb
+++ Formula/eask-cli.rb
@@ -2,7 +2,7 @@ cask "eask-cli" do
   arch arm: "arm64", intel: "x64"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
-  name: "Eask CLI"
+  name "Eask CLI"
   desc "A set of command-line tools to build Emacs packages"
   version "0.8.3"
   homepage "https://emacs-eask.github.io/"
