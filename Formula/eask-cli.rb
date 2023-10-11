85598957d4f9eac329f28fba7b52e7035e84a871
JenChieh <jcs090218@gmail.com>
2023-10-10 22:54:38 -0700

    spec version

--- Formula/eask-cli.rb
+++ Formula/eask-cli.rb
@@ -1,13 +1,14 @@
 require "language/node"
 
 class EaskCli < Formula
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-
   name "Eask CLI"
   desc "A set of command-line tools to build Emacs packages"
   version "0.8.3"
   homepage "https://emacs-eask.github.io/"
   license "GPLv3"
+
+  url    "https://registry.npmjs.org/@emacs-eask/-/cli-#{version}.tgz"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
   depends_on "node"
 
