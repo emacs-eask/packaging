f4152373252c195cf3f06f92a0bb4d5284e4b627
JenChieh <jcs090218@gmail.com>
2023-10-16 22:44:01 -0700

    checksum

--- scripts/_prepare.el
+++ scripts/_prepare.el
@@ -14,15 +14,15 @@
   "Return file's string."
   (with-temp-buffer (insert-file-contents file) (buffer-string)))
 
-(defun checksum-zip (symbol version arch)
+(defun checksum-zip (type version arch)
   "Return checksum zip path."
-  (cl-case symbol
+  (cl-case type
     (`npm (format "cli-%s.tgz" version))
     (t    (format "eask_%s_%s.zip" version arch))))
 
-(defun checksum-data (symbol version &optional arch)
-  "Return checksum data from SYMBOL and eask's VERSION."
-  (let* ((zip (checksum-zip symbol version arch))
+(defun checksum-data (type version &optional arch)
+  "Return checksum data from TYPE and eask's VERSION."
+  (let* ((zip (checksum-zip type version arch))
          (sha256 (format "checksum/%s/%s/sha256" version zip))
          (rmd160 (format "checksum/%s/%s/rmd160" version zip))
          (size   (format "checksum/%s/%s/size" version zip)))
--- scripts/update-checksum.el
+++ scripts/update-checksum.el
@@ -4,34 +4,34 @@
 
 (load-file "./scripts/_prepare.el")
 
-(defun openssl-command (symbol zip)
-  "Return openssl command by SYMBOL and ZIP."
+(defun openssl-command (type zip)
+  "Return openssl command by TYPE and ZIP."
   (cond
    (elenv-macos
-    (cl-case symbol
+    (cl-case type
       (:sha256 (format "openssl dgst -sha256 %s" zip))
       (:rmd160 (format "openssl dgst -rmd160 %s" zip))
       (:size   (format "stat -f%%z %s" zip))))
    (elenv-linux
-    (cl-case symbol
+    (cl-case type
       (:sha256 (format "openssl dgst -sha256 %s" zip))
       (:rmd160 (format "openssl dgst -rmd160 %s" zip))
       (:size   (format "stat -f%%z %s" zip))))
    (elenv-windows
-    (cl-case symbol
+    (cl-case type
       (:sha256 (format "Get-FileHash %s -Algorithm SHA256 | select -ExpandProperty Hash" zip))
       (:rmd160 (format "Get-FileHash %s -Algorithm RIPEMD160 | select -ExpandProperty Hash" zip))
       (:size   (format "Get-Item %s | select -ExpandProperty Length" zip))))))
 
-(defun openssl-parse-output (output)
-  "Extract hash from output."
+(defun openssl-parse-output (type output)
+  "Extract hash from OUTPUT by TYPE."
   (cond
    (elenv-macos
-    (cl-case symbol
+    (cl-case type
       ((or :sha256 :rmd160) (nth 1 (split-string output " ")))
       (:size                output)))
    (elenv-linux
-    (cl-case symbol
+    (cl-case type
       ((or :sha256 :rmd160) (nth 1 (split-string output " ")))
       (:size                output)))
    (elenv-windows output)))
@@ -41,11 +41,11 @@
        (dir (file-name-nondirectory zip))
        (parent (format "./checksum/%s/%s/" ver dir))
        (rmd160 (shell-command-to-string (openssl-command :rmd160 zip)))
-       (rmd160 (openssl-parse-output rmd160))
+       (rmd160 (openssl-parse-output :rmd160 rmd160))
        (sha256 (shell-command-to-string (openssl-command :sha256 zip)))
-       (sha256 (openssl-parse-output sha256))
+       (sha256 (openssl-parse-output :sha256 sha256))
        (size   (shell-command-to-string (openssl-command :size zip)))
-       (size   (openssl-parse-output size)))
+       (size   (openssl-parse-output :size size)))
   (ignore-errors (make-directory parent t))
   (write-region rmd160 nil (concat parent "rmd160"))
   (write-region sha256 nil (concat parent "sha256"))
