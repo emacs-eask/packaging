ea6fc908f35b99a104b54ab34140d2f261919db8
Jen-Chieh <jcs090218@gmail.com>
2023-10-16 20:00:59 -0700

    escape %z

--- scripts/update-checksum.el
+++ scripts/update-checksum.el
@@ -9,7 +9,7 @@
        (parent (format "./checksum/%s/%s/" ver dir))
        (rmd160 (shell-command-to-string (format "openssl dgst -rmd160 %s" zip)))
        (sha256 (shell-command-to-string (format "openssl dgst -sha256 %s" zip)))
-       (size   (shell-command-to-string (format "stat -f%z %s" zip))))
+       (size   (shell-command-to-string (format "stat -f%%z %s" zip))))
   (ignore-errors (make-directory parent t))
   (write-region rmd160 nil (concat parent "rmd160"))
   (write-region sha256 nil (concat parent "sha256"))
