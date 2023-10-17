290fcdbaac14aa7f9d96ffe7b133bff51a95b40d
JenChieh <jcs090218@gmail.com>
2023-10-16 21:47:24 -0700

    Update brew hash

--- scripts/_prepare.el
+++ scripts/_prepare.el
@@ -2,6 +2,7 @@
 ;;; Commentary:
 ;;; Code:
 
+(require 'cl-lib)
 (require 'subr-x)
 
 ;;
@@ -11,11 +12,18 @@
   "Return file's string."
   (with-temp-buffer (insert-file-contents file) (buffer-string)))
 
-(defun checksum-data (path version)
-  "Return checksum data from PATH and eask's VERSION."
-  `( :sha256 ,(file-to-string (format "checksum/%s/" version))
-     :rmd160 ,(file-to-string )
-     :size   ,(file-to-string )))
+(defun checksum-zip (symbol version arch)
+  "Return checksum zip path."
+  (cl-case symbol
+    (`npm (format "cli-%s.tgz" version))
+    (t (format "eask_%s_%s.zip" version arch))))
+
+(defun checksum-data (symbol version &optional arch)
+  "Return checksum data from SYMBOL and eask's VERSION."
+  (let ((zip (checksum-zip symbol version arch)))
+    `( :sha256 ,(file-to-string (format "checksum/%s/%s/sha256" version zip))
+       :rmd160 ,(file-to-string (format "checksum/%s/%s/rmd160" version zip))
+       :size   ,(file-to-string (format "checksum/%s/%s/size" version zip)))))
 
 ;; Local Variables:
 ;; coding: utf-8
