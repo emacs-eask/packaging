e4477cccda9d1a2ed2d73c9236e758bb5f21435f
JenChieh <jcs090218@gmail.com>
2023-12-02 19:55:01 -0800

    push{

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -2,16 +2,17 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= $env:ChocolateyPackageName
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = "https://github.com/emacs-eask/cli/releases/download/$($env:ChocolateyPackageVersion)/eask_$($env:ChocolateyPackageVersion)_win-x64.zip"
 $url64      = "https://github.com/emacs-eask/cli/releases/download/$($env:ChocolateyPackageVersion)/eask_$($env:ChocolateyPackageVersion)_win-x64.zip"
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
-  url           = $url
   url64bit      = $url64
 
+<<<<<<< HEAD
   checksum      = 'c8437d4de56bf3a3c62bfe1f68ffc287403f8d354019f84e6ad5611c483cf35a'
+=======
+>>>>>>> 63ba213e (fix: Choco)
   checksumType  = 'sha256'
   checksum64    = 'c8437d4de56bf3a3c62bfe1f68ffc287403f8d354019f84e6ad5611c483cf35a'
 }
