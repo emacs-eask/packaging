5f8ebde3cae844c0b1a22b327215addb0bbd2626
Jen-Chieh Shen <jcs090218@gmail.com>
2024-12-19 11:52:19 -0800

    Update eask-cli.scm

--- guix/eask-cli.scm
+++ guix/eask-cli.scm
@@ -1,3 +1,35 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2024 Jen-Chieh Shen <jcs090218@gmail.com>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (gnu packages node-xyz)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages sqlite)
+  #:use-module (gnu packages python)
+  #:use-module (gnu packages web)
+  #:use-module (guix gexp)
+  #:use-module (guix packages)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system node))
+
+;;;
+;;; Please: Try to add new module packages in alphabetic order.
+;;;
+
 (define-public node-eask-cli
   (package
     (name "node-eask-cli")
@@ -19,3 +51,7 @@
     (description "CLI for building, running, testing, and managing
 your Emacs Lisp dependencies")
     (license license:gpl3+)))
+;;;
+;;; Avoid adding new packages to the end of this file. To reduce the chances
+;;; of a merge conflict, place them above in alphabetic order.
+;;;
