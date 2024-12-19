a37b45bf2628e5591d9def5e36cebef34c9bd357
Jen-Chieh Shen <jcs090218@gmail.com>
2024-12-19 07:34:02 -0800

    Create eask-cli.scm

---
+++ guix/eask-cli.scm
@@ -0,0 +1,21 @@
+(define-public node-env-variable
+  (package
+    (name "node-eask-cli")
+    (version "0.10.2")
+    (source
+      (origin
+        (method git-fetch)
+        (uri (git-reference
+               (url "https://github.com/emacs-eask/cli")
+               (commit version)))
+        (file-name (git-file-name name version))
+        (sha256
+         (base32
+          "0nnpxjxfhy4na7fixb7p3ww6ard5xgggfm83b78i333867r4gmsq"))))
+    (build-system node-build-system)
+    (arguments '(#:tests? #f)) ; No tests.
+    (home-page "https://github.com/bigpipe/env-variable")
+    (synopsis "Environment variables for Node with fallbacks")
+    (description "CLI for building, running, testing, and managing
+your Emacs Lisp dependencies")
+    (license license:gpl3+)))
